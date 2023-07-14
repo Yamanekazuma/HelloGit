a544f10fc3448baa3ac840bdb41af77377c411e8
Yamanekazuma <ka6ka911k0@outlook.jp>
2023-07-15 03:54:46 +0900

    Initial Commit

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def calc(a: int, b: int):
+    #TODO Not implemented yet
+    pass
+
+if __name__ == "__main__":
+    a = 3
+    b = 2
+    print(f"calc({a}, {b}) = {calc(a, b)}")
